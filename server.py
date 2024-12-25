47d3087b29863528a751b2044b7def62bbcdda45
fishkadealer228 <midav.06@mail.ru>
2024-12-25 22:37:43 +0300

    commit

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ app.config.from_object(__name__)
 api = Api(app)
 
 api.add_resource(GetTries, '/staff_api/user/get_tries/<string:user_id>')
-api.add_resource(GetCodes, 'staff_api/prize/codes')
+api.add_resource(GetCodes, '/staff_api/prize/codes')
 
 api.add_resource(UserLoading, '/staff_api/user/loading/<string:user_id>/<string:username>/<int:tries>')
 api.add_resource(UserWin, '/staff_api/user/win/<string:username>/<string:prize>')
